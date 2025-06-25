ae2403e25d1bbf7448c48a9530c6e24c343db59f
liuxiaohan <liuxiaohan@nucleisys.com>
2025-06-26 00:24:08 +0800

    print ecall func update

--- firmware/helloworld/sbi.h
+++ firmware/helloworld/sbi.h
@@ -1,36 +1,60 @@
 /* mostly come from riscv-spi document */
-#ifndef _SBI_H
-#define _SBI_H
+#ifndef _RISCV_SBI_H
+#define _RISCV_SBI_H
 
-#define SBI_SET_TIMER 0
-#define SBI_CONSOLE_PUTCHAR 1
-#define SBI_CONSOLE_GETCHAR 2
-#define SBI_CLEAR_IPI 3
-#define SBI_SEND_IPI 4
-#define SBI_REMOTE_FENCE_I 5
-#define SBI_REMOTE_SFENCE_VMA 6
-#define SBI_REMOTE_SFENCE_VMA_ASID 7
-#define SBI_SHUTDOWN 8
+typedef unsigned long int uintptr_t;
 
-#define SBI_ECALL(__num, __a0, __a1, __a2)                           \
-({                                                                  \
-    register unsigned long a0 asm("a0") = (unsigned long)(__a0);    \
-    register unsigned long a1 asm("a1") = (unsigned long)(__a1);    \
-    register unsigned long a2 asm("a2") = (unsigned long)(__a2);    \
-    register unsigned long a7 asm("a7") = (unsigned long)(__num);   \
-    asm volatile("ecall"                                            \
-                 : "+r"(a0)                                         \
-                 : "r"(a1), "r"(a2), "r"(a7)                        \
-                 : "memory");                                       \
-    a0;                                                             \
-})
+struct sbiret {
+	unsigned long error;
+	unsigned long value;
+};
 
-#define SBI_ECALL_0(__num) SBI_ECALL(__num, 0, 0, 0)
-#define SBI_ECALL_1(__num, __a0) SBI_ECALL(__num, __a0, 0, 0)
-#define SBI_ECALL_2(__num, __a0, __a1) SBI_ECALL(__num, __a0, __a1, 0)
+enum sbi_ext_id {
+	SBI_EXT_0_1_SET_TIMER = 0x0,
+	SBI_EXT_0_1_CONSOLE_PUTCHAR = 0x1,
+	SBI_EXT_0_1_CONSOLE_GETCHAR = 0x2,
+	SBI_EXT_0_1_CLEAR_IPI = 0x3,
+	SBI_EXT_0_1_SEND_IPI = 0x4,
+	SBI_EXT_0_1_REMOTE_FENCE_I = 0x5,
+	SBI_EXT_0_1_REMOTE_SFENCE_VMA = 0x6,
+	SBI_EXT_0_1_REMOTE_SFENCE_VMA_ASID = 0x7,
+	SBI_EXT_0_1_SHUTDOWN = 0x8,
 
-#define SBI_PUTCHAR(__a0) SBI_ECALL_1(SBI_CONSOLE_PUTCHAR, __a0)
-#define SBI_TIMER(__a0) SBI_ECALL_1(SBI_SET_TIMER, __a0)
+	SBI_EXT_BASE = 0x10,
+	SBI_EXT_TIME = 0x54494D45,
+	SBI_EXT_IPI = 0x735049,
+	SBI_EXT_RFENCE = 0x52464E43,
+	SBI_EXT_HSM = 0x48534D,
+	SBI_EXT_SRST = 0x53525354,
+	SBI_EXT_PMU = 0x504D55,
 
-#endif
+	/* Experimentals extensions must lie within this range */
+	SBI_EXT_EXPERIMENTAL_START = 0x08000000,
+	SBI_EXT_EXPERIMENTAL_END = 0x08FFFFFF,
+
+	/* Vendor extensions must lie within this range */
+	SBI_EXT_VENDOR_START = 0x09000000,
+	SBI_EXT_VENDOR_END = 0x09FFFFFF,
+};
+
+enum sbi_ext_base_fid {
+	SBI_EXT_BASE_GET_SPEC_VERSION = 0,
+	SBI_EXT_BASE_GET_IMP_ID,
+	SBI_EXT_BASE_GET_IMP_VERSION,
+	SBI_EXT_BASE_PROBE_EXT,
+	SBI_EXT_BASE_GET_MVENDORID,
+	SBI_EXT_BASE_GET_MARCHID,
+	SBI_EXT_BASE_GET_MIMPID,
+};
+
+struct sbiret sbi_ecall(int ext, int fid, unsigned long arg0,
+			unsigned long arg1, unsigned long arg2,
+			unsigned long arg3, unsigned long arg4,
+			unsigned long arg5);
+
+
+
+void sbi_console_putchar(int ch);
+
+#endif  /* _RISCV_SBI_H */
 
